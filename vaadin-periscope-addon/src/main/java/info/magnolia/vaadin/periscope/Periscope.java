0f372d0d13555ab651dc75c222839735bae58316
Cedric Reichenbach <cedric.reichenbach@magnolia-cms.com>
2017-05-30 13:01:20 +0200

    Start integrating speech recognition

--- vaadin-periscope-addon/src/main/java/info/magnolia/vaadin/periscope/Periscope.java
+++ vaadin-periscope-addon/src/main/java/info/magnolia/vaadin/periscope/Periscope.java
@@ -33,20 +33,26 @@
  */
 package info.magnolia.vaadin.periscope;
 
+import info.magnolia.speech.GoogleSpeechRecognitionService;
 import info.magnolia.vaadin.periscope.result.AsyncResultSupplier;
 import info.magnolia.vaadin.periscope.result.Result;
 import info.magnolia.vaadin.periscope.result.ResultSupplier;
+import info.magnolia.vaadin.speech.AudioRecorder;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.vaadin.event.FieldEvents;
 import com.vaadin.event.ShortcutAction;
 import com.vaadin.event.ShortcutListener;
 import com.vaadin.ui.AbstractTextField;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
@@ -90,6 +96,8 @@ public class Periscope extends VerticalLayout {
             // TODO: Find a way to blur input (remove focus)
         }));
         input.addBlurListener(event -> resultList.clearSelector());
+
+        this.addComponent(createSpeechButton());
     }
 
     private void inputChanged(FieldEvents.TextChangeEvent event) {
@@ -135,5 +143,39 @@ public class Periscope extends VerticalLayout {
                 action.run();
             }
         };
+    }
+
+    private Component createSpeechButton() {
+        final AudioRecorder audioRecorder = new AudioRecorder();
+
+        final Button startStopButton = new Button("Start");
+        final AtomicBoolean isRecording = new AtomicBoolean(false);
+
+        startStopButton.addClickListener((Button.ClickListener) event -> {
+            if (isRecording.get()) {
+                startStopButton.setCaption("Start");
+                audioRecorder.stop();
+            } else {
+                startStopButton.setCaption("Stop");
+                audioRecorder.record();
+            }
+            isRecording.set(!isRecording.get());
+        });
+
+        final GoogleSpeechRecognitionService googleSpeechRecognitionService = new GoogleSpeechRecognitionService();
+
+        audioRecorder.addValueChangeListener((AudioRecorder.ValueChangeListener) wavBinary -> {
+            try {
+                final String speech = googleSpeechRecognitionService.recognise(wavBinary);
+                input.setValue(speech);
+                System.out.println("GREAT SUCCESS, USER SAID: " + speech);
+                // TODO
+            } catch (IOException e) {
+                System.err.println("Failed to recognize a piece of recorded audio");
+                e.printStackTrace();
+            }
+        });
+
+        return new VerticalLayout(startStopButton, audioRecorder);
     }
 }
